339d70f35f94f992f5e0633ab14722b13f6bed2a
Mersi <mersi.tesfaye18@gmail.com>
2025-03-10 11:36:34 -0700

    Navbar component updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Navbar from './components/Navbar/Nav'
+import Nav from './components/Navbar/Nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Nav />
     </div>
   );
 }
--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,8 +1,20 @@
-function Navbar(){
+import React from 'react';
+ const Nav = () =>{
     return(
-        <div className="text-5xl text-red text-violet-50">
-            hello from navbar
+        <div className="bg-gradient-to-r from-slate-600 to-slate-900 text-white flex items-center justify-between">
+            <div className="px-[20px]">
+                Logo
+            </div>
+            <ul className="md:flex flex-row h-16 items-center mr-5 text-[15px]">
+                <li className="px-3 hover:text-white ease-in"><a href="#" className='w-50 hover:bg-slate-200 hover:text-black  duration-300 ease-in p-2 rounded'>Home</a></li>
+                <li className="px-3 hover:text-red transition duration-150 ease-out hover:ease-in"><a href="#">Skills</a></li>
+                <li className="px-3"><a href="#">Projects</a></li>
+                <li className="px-3"><a href="#">Testimonials</a></li>
+                <li className="px-3"><a href="#">Contact</a></li>
+            </ul>
+          
+
         </div>
     )
 }
-export default Navbar;
+export default Nav;
