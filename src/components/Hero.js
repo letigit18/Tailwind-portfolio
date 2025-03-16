ac6061f398f93deec8e1c0fb54b6f77e1e630cb1
Mersi <mersi.tesfaye18@gmail.com>
2025-03-16 10:30:03 -0700

    Hero component updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,17 @@
 import React from 'react'
 const Hero = ()=>{
     return(
-        <div className='w-full h-28 bg-blue-950'>
-            fdfd
+        <div className='w-full h-[60vh] bg-slate-100 p-5 flex justify-between items-center'>
+            <div className='flex flex-col space-y-2'>
+                <b className='text-lg'>Hi, I'm Letera Tesfaye, Full Stack Developer</b>
+                <p>Skills</p>
+                <div>
+                    <div>djfkd</div>
+                </div>
+            </div>
+            <div>
+                My picture 
+            </div>
         </div>
     )
 }
