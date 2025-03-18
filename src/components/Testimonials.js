217ac954ef6d36e8c34f323260e6c44cef777cc9
Mersi <mersi.tesfaye18@gmail.com>
2025-03-18 06:13:16 -0700

    Testimonials page updated

---
+++ src/components/Testimonials.js
@@ -0,0 +1,47 @@
+import React from 'react'
+const Testimonials = ()=>{
+    return(
+        <div className='font-Poppins w-full py-5 h-[400px] bg-slate-50'>
+            <p className='text-center text-lg font-bold'>Clients Testimonial</p>
+            <div className='flex gap-5 p-[20px]'>
+                <div className='p-[10px] border-[1px] border-red-100 bg-white h-[250px] w-[350px] rounded-lg shadow-md hover:scale-105 transition-transform ease-in duration-300'>
+                  <div>
+                    <p className='text-yellow-500 text-center'>&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                    <p className='text-slate-700 text-sm'>"Great experience working with Letera. He was punctual, delivered high-quality work, and met 
+                        my expectations. The React landing page for my SaaS turned out exactly as I envisioned. I will definitely hire 
+                        him again" 
+                    </p>
+                  </div>
+                  <div className='flex text-sm justify-between items-center mt-[10px]'>
+                    <div className='text-left'>
+                        <b>Seifu Zerihun(PhD.)</b>
+                        <p><b>Principal</b> at Kaytics</p>
+                        <p>+1(217)5129157</p>
+                        <p>USA</p>
+                    </div>
+                    <img src="seifu.png" className='w-[70px] h-[70px] rounded-full'/>
+                  </div>
+                   
+                </div>
+                <div className='p-[10px] border-[1px] border-red-100 bg-white h-[250px] w-[350px] rounded-lg shadow-md hover:scale-105 transition-transform ease-in duration-300'>
+                  <div>
+                    <p className='text-yellow-500 text-center'>&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                    <p className='text-slate-700 text-sm'>"Mr. Letera Tesfaye you have contributed a lot to our company in terms of your proffesionalism. We appreciate your creativeness, punctuality to take off the task and your presentation on the system for our team was unforgettable. Hope you will bring great to our country in tech. Sector" 
+                    </p>
+                  </div>
+                  <div className='flex text-sm justify-between items-center mt-[10px]'>
+                    <div className='text-left'>
+                        <b>Gadisa Bekole</b>
+                        <p><b>Manager, </b>Tigist Gizawu VAS</p>
+                        <p>+251911507701</p>
+                        <p>Addis Ababa, Ethiopia</p>
+                    </div>
+                    <img src="seifu.png" className='w-[70px] h-[70px] rounded-full'/>
+                  </div>
+                   
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Testimonials;
